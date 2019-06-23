87b5f7ea25b0271cab99d5a0662f2f27dd6821c4
Njeri Ngigi <njery.ngigi@gmail.com>
2019-06-23 15:35:53 +0200

    Updates

---
+++ precache-manifest.89f95b6ed2a4459f52facd689f97de01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee803a94e3513a44147e7ceabd366da",
+    "url": "/TODO-react-app/index.html"
+  },
+  {
+    "revision": "6eadbec50d0f831925c5",
+    "url": "/TODO-react-app/static/css/main.eb5919cb.chunk.css"
+  },
+  {
+    "revision": "3fca17d88530561c09de",
+    "url": "/TODO-react-app/static/js/2.c2738798.chunk.js"
+  },
+  {
+    "revision": "6eadbec50d0f831925c5",
+    "url": "/TODO-react-app/static/js/main.5a7253f2.chunk.js"
+  },
+  {
+    "revision": "64e892e5a508686eeb3e",
+    "url": "/TODO-react-app/static/js/runtime~main.fcb8dd4e.js"
+  }
+]);
